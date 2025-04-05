c3b29b4cc9a62dd9e6deb00eb8aaa06dfdc0efda
MSpilari <mb.spilari@hotmail.com>
2025-04-05 13:58:00 -0300

    feat: Create AlbumRepository with uploadoImage saveAlbum, getAlbumId and getAllAlbums

---
+++ webSocketBackend/src/main/java/dev/MSpilari/webSocketBackend/repositories/AlbumRepository.java
@@ -0,0 +1,49 @@
+package dev.MSpilari.webSocketBackend.repositories;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import dev.MSpilari.webSocketBackend.models.Album;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+
+@Repository
+public class AlbumRepository {
+
+    private final DynamoDbTable<Album> albumTable;
+
+    public AlbumRepository(DynamoDbEnhancedClient enhancedClient,
+            @Value("${spring.cloud.aws.dynamodb.table-name}") String tableName) {
+        this.albumTable = enhancedClient.table(tableName, TableSchema.fromBean(Album.class));
+    }
+
+    public void saveAlbum(List<String> picturesUrl, String albumName) {
+        String albumId = generateAlbumId();
+
+        Album newAlbum = new Album(albumId, albumName, picturesUrl);
+
+        albumTable.putItem(newAlbum);
+    }
+
+    private String generateAlbumId() {
+        Instant now = Instant.now();
+        return DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS")
+                .withZone(ZoneOffset.UTC)
+                .format(now);
+    }
+
+    public Optional<Album> getAlbumById(String albumId) {
+        return Optional.ofNullable(albumTable.getItem(r -> r.key(k -> k.partitionValue(albumId))));
+    }
+
+    public List<Album> getAllAlbums() {
+        return albumTable.scan().items().stream().toList();
+    }
+}
