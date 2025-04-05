23dacfb47dad8d9ddf475a7d1c9ac4ed9f53b7c9
MSpilari <mb.spilari@hotmail.com>
2025-04-05 13:48:49 -0300

    first commit on tests

---
+++ webSocketBackend/src/test/java/dev/MSpilari/webSocketBackend/WebSocketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.MSpilari.webSocketBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
