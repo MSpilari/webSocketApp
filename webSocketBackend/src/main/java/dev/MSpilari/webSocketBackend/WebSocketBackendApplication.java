dc9555a46686864d2db9e9e8b1c854f30719ab34
MSpilari <mb.spilari@hotmail.com>
2025-04-05 13:58:42 -0300

    feat: Create application entry point

---
+++ webSocketBackend/src/main/java/dev/MSpilari/webSocketBackend/WebSocketBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.MSpilari.webSocketBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketBackendApplication.class, args);
+	}
+
+}
