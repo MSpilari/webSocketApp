e003288d2e953cb999f8d68ab0eb08eaa7cea75b
MSpilari <mb.spilari@hotmail.com>
2025-04-17 17:55:37 -0300

    feat: insert websocketService

--- webSocketBackend/src/main/java/dev/MSpilari/webSocketBackend/services/AlbumServices.java
+++ webSocketBackend/src/main/java/dev/MSpilari/webSocketBackend/services/AlbumServices.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import dev.MSpilari.webSocketBackend.models.Album;
 import dev.MSpilari.webSocketBackend.repositories.AlbumRepository;
 import dev.MSpilari.webSocketBackend.repositories.S3Repository;
 
@@ -16,10 +17,13 @@ public class AlbumServices {
 
     private final S3Repository s3Repository;
     private final AlbumRepository albumRepository;
+    private final AlbumWebSocketServices webSocketService;
 
-    public AlbumServices(S3Repository s3Repository, AlbumRepository albumRepository) {
+    public AlbumServices(S3Repository s3Repository, AlbumRepository albumRepository,
+            AlbumWebSocketServices webSocketService) {
         this.s3Repository = s3Repository;
         this.albumRepository = albumRepository;
+        this.webSocketService = webSocketService;
     }
 
     public String uploadAlbum(MultipartFile[] images, String albumName) throws IOException {
@@ -36,7 +40,8 @@ public class AlbumServices {
             listOfUrls.add(pictureUrl);
         }
 
-        albumRepository.saveAlbum(listOfUrls, albumName);
+        Album newAlbum = albumRepository.saveAlbum(listOfUrls, albumName);
+        webSocketService.broadcastNewAlbum(newAlbum);
 
         return "Album created successfully !";
 
