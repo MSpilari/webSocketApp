dc03bb7e3b21ec96ef774bc70cb343d05677ddf9
MSpilari <mb.spilari@hotmail.com>
2025-04-17 18:05:45 -0300

    feat: add AlbumList component

--- socketfrontend/src/App.tsx
+++ socketfrontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import AlbumForm from "./components/AlbumForm/AlbumForm";
+import AlbumList from "./components/AlbumList/AlbumList";
 
 function App() {
   return (
     <main>
+      <AlbumList />
       <AlbumForm />
     </main>
   );
