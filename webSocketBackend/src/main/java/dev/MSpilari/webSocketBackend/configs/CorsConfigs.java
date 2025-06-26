1eafb744a9d035a54564124a5d33842b773dcd67
MSpilari <mb.spilari@hotmail.com>
2025-06-26 16:39:16 -0300

    feat: insert environment variables into WebSocketConfig and CorsConfigs

--- webSocketBackend/src/main/java/dev/MSpilari/webSocketBackend/configs/CorsConfigs.java
+++ webSocketBackend/src/main/java/dev/MSpilari/webSocketBackend/configs/CorsConfigs.java
@@ -1,5 +1,6 @@
 package dev.MSpilari.webSocketBackend.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfigs {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +19,7 @@ public class CorsConfigs {
             @Override
             public void addCorsMappings(CorsRegistry corsRegistry) {
                 corsRegistry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
